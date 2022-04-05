4170043e9364a2babe5ea3ef4dedabcf1cd27125
Vaieti <100234698+Vaieti@users.noreply.github.com>
2022-04-05 16:07:01 -0400

    4f

---
+++ final_project/Assets/Scripts/Formule4facteurs.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Formule4facteurs : MonoBehaviour
+{
+    public double epsilon = 0;
+    public double nu = 0;
+    public double p = 0;
+    public double f = 0;
+    public double Kinf = 0;
+    public double keff = 0;
+    public double enrichissement = 0;
+    public double moderation = 0;
+    public double fuite = 0;
+    public double D = 0;
+    public double H = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    double calculFuite()
+    {
+        double M = 0.45;
+        double B = 0;
+        fuite = 1 / (10);
+        return fuite;
+    }
+
+    double calculF()
+    {
+        double phi = 0;
+        phi = 4.901 * Mathf.Pow((float)10, -4) * ((moderation + 0.3624) / (enrichissement + 3.957 * Mathf.Pow((float)10, -3)));
+        f = 1 / (1 + phi);
+
+        return f;
+    }
+
+    double calculP()
+    {
+        float z = 0f;
+        float N = 0f;
+        N = (float)(enrichissement *1.42857* Mathf.Pow(10, 21));
+        z = (float)(5.104 * Mathf.Pow(10, -12) * Mathf.Pow(N, (float)0.522) * Mathf.Pow((float)(moderation + 0.01), -1));
+        p = (Mathf.Pow((float)2.71828, -z));
+
+        return p;
+    }
+
+    double calculNu()
+    {
+        nu = (2.082 / (1 + 3.957 / enrichissement));
+
+        return nu;
+    }
+
+    double calculEpsilon()
+    {
+        float temp = 0f;
+        temp = (float)(-0.0546 * moderation);
+        epsilon = (1 + ((1 - p) / p) * (1 / f) * (0.137*Mathf.Pow((float)2.71828, temp)));
+
+        return epsilon;
+    }
+}
