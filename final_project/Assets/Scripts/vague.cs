cf9b7eeb26c5a252ff5beb714913f4eac3bf870b
astro727 <88118613+astro727@users.noreply.github.com>
2022-05-02 19:39:14 -0400

    mouvement Eau

--- final_project/Assets/Scripts/vague.cs
+++ final_project/Assets/Scripts/vague.cs
@@ -13,35 +13,64 @@ public class vague : MonoBehaviour
     public float destroy_X = 0f;
     public float destroy_Y = 0f;
     public float destroy_Z = 0f;
+
+
+
+    [SerializeField] Vector3[] deplacement;
+    [SerializeField] [Range(0f, 100f)] float lerpTime;
+    public float elapsedTime;
+    public float desiredDuration = 5f;
+    private Vector3 startPosition;
+
+    int pos = 0;
+    int lenght;
+    float t = 0f;
+
+
     // public Vector3 position;
     // Start is called before the first frame update
     void Start()
     {
-
+        lenght = deplacement.Length;
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         moveObject();
-        destroyGameObject();
+        //destroyGameObject();
     }
 
     void moveObject()
     {
+        elapsedTime += Time.deltaTime;
+        float percentageChange = elapsedTime / lerpTime;
+        transform.position = Vector3.Lerp(startPosition, deplacement[pos], percentageChange);
+
+        t = Mathf.Lerp(t, 1f, lerpTime * Time.deltaTime);
+        if(t>.9f)
+        {
+            t = 0f;
+            pos++;
+            pos = (pos >= lenght) ? 0 : pos;
+        }
        
+        
+        
         Vector3 position = transform.position;
         float speed_x = Mathf.Abs(position_X / position_Y);
         float speed_y = Mathf.Abs(position_Y / position_X);
-        
+
         //position.x -= speed_x * speed * Time.deltaTime;
-        //position.z -= speed_y * speed * Time.deltaTime;
+       // position.z -= speed_y * speed * Time.deltaTime;
 
-        position.x -= speed * Time.deltaTime;
-        position.z -= speed * Time.deltaTime;
+        
+//position.x -= speed * Time.deltaTime;
+  //position.z -= speed * Time.deltaTime;
 
 
-        transform.position = position;
+        //transform.position = position;
     }
 
     void destroyGameObject()
@@ -54,22 +83,4 @@ public class vague : MonoBehaviour
             
     }
 
-    void turnOffGameObject()
-    {
-        gameObject.SetActive(false);
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        Invoke("turnOffGameObject",1f);
-        if(other.tag == "deleteBox")
-        {
-            Destroy(this.gameObject);
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        
-    }
 }
