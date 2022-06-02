716cc66d620258fbbb1e9b8b95b756a88ecb0d1f
astro727 <88118613+astro727@users.noreply.github.com>
2022-06-02 07:16:55 -0400

    Update toolTipTrigger.cs

--- final_project/Assets/Scripts/toolTipTrigger.cs
+++ final_project/Assets/Scripts/toolTipTrigger.cs
@@ -19,13 +19,19 @@ public class toolTipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitH
     // Permet de choisir l'action lorsque l'utilisateur enlève son curseur sur le texte. Dans notre cas, nous avons décidé d'enlever le tooltip
     public void OnPointerExit(PointerEventData eventData)
     {
+        //désactive la variable bool pour annuler l'affichage si l'utilisateur quite la zone
         time = false;
         tooltipSystem.Hide();
     }
+<<<<<<< HEAD
 
     // Permet de choisir le delai d'affichage après avoir mis notre curseur
+=======
+     
+>>>>>>> d05c401c552a097e5c7284f6e090505cc6ade832
     IEnumerator delayedCall()
     {
+        //ajoute un délai court avant l'affichage
         time = true;
         yield return new WaitForSeconds(0.5f);
         if(time == true)
